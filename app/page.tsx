e81bb4b24fe847ea2d98fbe7a18e94c5b785ed60
AllanC12 <allandevfront@gmail.com>
2024-03-21 22:50:12 -0300

    definindo e aplicando animações iniciais

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import './globals.css'
+import Curtain from '../components/Curtain';
 import Banner from "../components/Banner";
 
 export default function Home() {
   return (
-    <>
+    <div className="page_home">
+      <Curtain/>
       <Banner/>
-    </>
+    </div>
 
   );
 }
--- components/Slogan.tsx
+++ components/Slogan.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect,useRef } from "react";
 
 import styles from "../components/sass_components/Slogan.module.css";
 
@@ -11,20 +11,28 @@ import ButtonBanner from "./buttons/ButtonBanner";
 import ButtonContacts from "./buttons/ButtonContacts";
 
 const Slogan = () => {
+
+  const span_one = useRef<HTMLSpanElement>(null)
+  const span_two = useRef<HTMLSpanElement>(null)
+  const span_three = useRef<HTMLSpanElement>(null)
+  const listRef: React.MutableRefObject<HTMLSpanElement | null>[] = [span_one, span_two, span_three]
+
   useEffect(() => {
-    AOS.init({ duration: 1500, delay: 100 });
+    AOS.init({ duration: 1000});
   }, []);
+   
+
 
   return (
     <>
       <p className={styles.slogan_banner}>
-        <span className={styles.span_slogan} data-aos="fade-right">
+        <span ref={span_one} className={styles.span_slogan} data-aos="fade-right">
           Arquitetura que ecoa a história,
         </span>
-        <span className={styles.span_slogan} data-aos="fade-left">
+        <span ref={span_two} className={styles.span_slogan} data-aos="fade-left">
           celebra o presente
         </span>
-        <span className={styles.span_slogan} data-aos="fade-right">
+        <span ref={span_three} className={styles.span_slogan} data-aos="fade-right">
           e vislumbra o futuro.
         </span>
       </p>

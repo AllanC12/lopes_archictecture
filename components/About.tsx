6ecc2c729c54812b864e495360f846a2c5e9b356
AllanC12 <allandevfront@gmail.com>
2024-04-08 23:02:39 -0300

    manipulando exibição de menu

---
+++ components/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from '../components/sass_components/About.module.scss';
+
+type Props = {}
+
+const About = (props: Props) => {
+  return (
+    <div id="about" className={styles.about}>About</div>
+  )
+}
+
+export default About
---
+++ components/Contacts.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from '../components/sass_components/Contacts.module.css';
+
+type Props = {}
+
+const Contacts = (props: Props) => {
+  return (
+    <div id="contacts"className={styles.contacts}>Contacts</div>
+  )
+}
+
+export default Contacts

5eb3cd5fbce6705791d76b3f5147f37af61059d8
AllanC12 <allandevfront@gmail.com>
2024-04-20 22:51:14 -0300

    alterando header e corrigindo bug de animação da cortina

--- components/buttons/SocialMediaButtons.tsx
+++ components/buttons/SocialMediaButtons.tsx
@@ -1,16 +1,28 @@
+'use client'
+
+import { useEffect } from "react";
+
 import Link from "next/link";
 
 import styles from "../buttons/sass_buttons/SocialMediaButtons.module.scss";
+
+import { initAnimations } from "@/aos/aos";
 
 import { FaWhatsapp } from "react-icons/fa6";
 import { MdOutlineMailOutline } from "react-icons/md";
 
 const SocialMediaButtons = () => {
+
+  useEffect(() => {
+    initAnimations()
+  },[])
+  
   return (
     <>
       <div className={styles.card}>
 
         <Link
+        data-aos="fade-right"
           className={`${styles.socialContainer} ${styles.containerTwo}  ${styles.container_icon}`}
           href="#"
         >
@@ -19,6 +31,7 @@ const SocialMediaButtons = () => {
 
 
         <Link
+          data-aos="fade-left"
           className={`${styles.socialContainer} ${styles.containerFour}  ${styles.container_icon}`}
           href="#"
         >

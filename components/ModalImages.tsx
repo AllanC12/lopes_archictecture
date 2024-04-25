69e8cee2de01ee225f2eaa1ab18be39107f9dd28
AllanC12 <allandevfront@gmail.com>
2024-04-24 23:04:39 -0300

    aplicando borda em imagem individual

--- components/ModalImages.tsx
+++ components/ModalImages.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SetStateAction, useRef,useState } from "react";
+import { SetStateAction, useRef,useState, useEffect } from "react";
 
 import Image from "next/image";
 
@@ -17,18 +17,21 @@ interface propModal {
 }
 
 const ModalImages = ({ setShowModal, project }: propModal) => {
+  
+  const refBoxImage = useRef<HTMLDivElement[]>([])
 
+  
   const listImages: string[] | undefined = project?.images
   const [indexMaxImage] = useState<number>(listImages!.length)
   const [indexCurrentImage,setIndexCurrentImage] = useState<number>(0)
-
+  
   const showNextImage = () => {
     if(indexCurrentImage === indexMaxImage - 1){
       return
     }
     setIndexCurrentImage(indexCurrentImage + 1)
-   }
-
+  }
+  
   const showPreviousImage = () => {
     if(indexCurrentImage === 0){
       return
@@ -36,7 +39,18 @@ const ModalImages = ({ setShowModal, project }: propModal) => {
     setIndexCurrentImage(indexCurrentImage - 1)
   }
 
+  const applyBorder = (element: HTMLDivElement) => {
+    const image = element.children[0] as HTMLElement
+    image.style.setProperty("border",'4px solid #000')
+  }
+  
+  useEffect(() => {
+    refBoxImage.current = refBoxImage.current?.slice(0,listImages!.length)
+    applyBorder(refBoxImage.current[indexCurrentImage])
+  })
+
   return (
+    
     <div className={styles.modal}>
       <div className={styles.closeModal}>
         <IoMdClose onClick={() => setShowModal(false)} />
@@ -53,7 +67,11 @@ const ModalImages = ({ setShowModal, project }: propModal) => {
 
         <div className={styles.slide_secondary}>
             {listImages?.map((srcImage: string,index:number) => (
-                <div key={index} className={styles.box_image}>
+                <div 
+                 key={index} 
+                 className={styles.box_image}
+                 ref={(element) => element && (refBoxImage.current[index] = element) }
+                 >
                   <Image src={srcImage} width={100} height={100} alt="Imagem de projeto" />
                 </div>
             ))}

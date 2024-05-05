2c4eb9ec5810d818aa5de0fa843f9e7a63b953ae
AllanC12 <allandevfront@gmail.com>
2024-05-05 20:02:33 -0300

    correções de responsividade

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <div className="page_home">
       <Header />
-      <Curtain />
+      {/* <Curtain /> */}
       <Banner />
       <Projects />
       <About />

ffafcb952ef401a47d9555b32d60507ce151f182
AllanC12 <allandevfront@gmail.com>
2024-05-05 20:04:20 -0300

    eliminando componente desnecessário e ativando animação inicial

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Header from "@/components/Header";
 import Curtain from "@/components/Curtain";
 import Banner from "@/components/Banner";
 import Projects from "@/components/Projects";
-import Contacts from "@/components/Contacts";
 import About from "@/components/About";
 import Footer from "@/components/Footer";
 
@@ -22,11 +21,10 @@ export default function Home() {
   return (
     <div className="page_home">
       <Header />
-      {/* <Curtain /> */}
+      <Curtain />
       <Banner />
       <Projects />
       <About />
-      {/* <Contacts /> */}
       <Footer />
     </div>
   );

a9672b248c09eab13834e2a0dcdac292b1c77c24
AllanC12 <allandevfront@gmail.com>
2024-06-29 14:26:07 -0300

    correção de z-index e viewport

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
 import "./globals.css";
 
-import { Viewport } from "next";
-
 import Header from "@/components/Header";
 import Curtain from "@/components/Curtain";
 import Banner from "@/components/Banner";
 import Projects from "@/components/Projects";
 import About from "@/components/About";
 import Footer from "@/components/Footer";
-
-export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-}
 
 export default function Home() {
   return (

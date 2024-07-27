322f4a3fc48952e39092a2826696cf93edb5ad88
AllanC12 <allandevfront@gmail.com>
2024-07-27 20:58:43 -0300

    alterando titulo

--- components/Curtain.tsx
+++ components/Curtain.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Braah_One } from "next/font/google";
 import styles from "../components/sass_components/Curtain.module.css";
 
 import { useRef,useEffect,useState } from "react";
@@ -33,7 +34,12 @@ const Curtain = () => {
     <div ref={curtain} className={styles.curtain}>
       <div className={styles.text_curtain}>
         <span></span>
-        <span>{pageLoaded && "LOPES ARCHITECTURE"}</span>
+        <span>{pageLoaded && "PORTFÓLIO DE ARQUITETURA"}</span>
+        <span></span>
+      </div>
+      <div className={`${styles.text_curtain} ${styles.text_curtain_two}`}>
+        <span></span>
+        <span>{pageLoaded && "por Carlos Eduardo Lopes"}</span>
         <span></span>
       </div>
     </div>

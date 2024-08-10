28822e741aee170553a0124165cce42950109b8a
AllanC12 <allandevfront@gmail.com>
2024-08-09 22:54:35 -0300

    retirando dados mockados da sessão de projetos

--- app/projects/projects.ts
+++ app/projects/projects.ts
@@ -1,6 +1,12 @@
 export interface IProject {
     id: number
     name: string;
+    location: string;
+    lat: string;
+    terrain_area: string;
+    build_area: string;
+    permeable_area: string;
+    software: string;
     linkForDrive: string;
     images: string[];
     description: string;
@@ -8,115 +14,31 @@ export interface IProject {
 
 export const listProjects: IProject[] = [
     { id: 1,
-      name: "Elisian Heights Residence",
-      linkForDrive: "https://www.googledrive.com/lopesarchitecture/elisian",
+      name: "Casa Boa Vista",
+      location: "São Francisco de Paula - Bairro Explanada - MG - Brasil",
+      lat: "-20.7",
+      terrain_area: "221,5 m²",
+      build_area: "130 m²",
+      permeable_area: "29,5 m²",
+      software: "AutoCAD e SketchUp",
+      linkForDrive: "https://drive.google.com/drive/folders/1siw3Q5QHrdjMyKWcLjG4RrOsvCpTtotO?usp=drive_link ",
       images: [
-        "https://images5.alphacoders.com/484/thumb-1920-484403.jpg",
-        "https://avatarfiles.alphacoders.com/116/116791.jpg",
-        "https://images3.alphacoders.com/168/168629.jpg",
-        "https://images5.alphacoders.com/484/thumb-1920-484403.jpg",
-        "https://avatarfiles.alphacoders.com/116/116791.jpg",
-        "https://images3.alphacoders.com/168/168629.jpg",
-        "https://images5.alphacoders.com/484/thumb-1920-484403.jpg",
-        "https://avatarfiles.alphacoders.com/116/116791.jpg",
-        "https://images3.alphacoders.com/168/168629.jpg",
-        "https://images5.alphacoders.com/484/thumb-1920-484403.jpg",
-        "https://avatarfiles.alphacoders.com/116/116791.jpg",
-        "https://images3.alphacoders.com/168/168629.jpg",
+        "https://drive.google.com/uc?export=view&id=1abNiPIwTd6IIUilszRTNH8Isrrx7ZY4l",
+        "https://drive.google.com/uc?export=view&id=1YOLkJH2d3WGmqNlcXhOSQC_8m_myLuTZ",
+        "https://drive.google.com/uc?export=view&id=1I_ozzGbvd_2nVNh-zKsKhZQwEaItFy46",
+        "https://drive.google.com/uc?export=view&id=1rg7wgbcFAN3m7KKMxSE0IOetjdRZwshz",
+        "https://drive.google.com/uc?export=view&id=16ovB6pt5s_Qb1e8O6bLIKwYmuwwXhlS1",
+        "https://drive.google.com/uc?export=view&id=1HtPdOlDG4cU-fEMKMmCTFMTi8WdQ2Hcd",
+        "https://drive.google.com/uc?export=view&id=1VqmAkqTGRuWDCX5eZ-7SgRa1JnGMnCI3",
+        "https://drive.google.com/uc?export=view&id=1pLrX1-vOAeHh4U0hSqhMUx66NSNhlqmd",
+        "https://drive.google.com/uc?export=view&id=1Em67Disuky6iqZ3Yprr3hdipwiTF_ph5",
+        "https://drive.google.com/uc?export=view&id=1YOLkJH2d3WGmqNlcXhOSQC_8m_myLuTZ",
+        "https://drive.google.com/uc?export=view&id=1bElX6naLbRbKRYU9g5XeUsIe5gZHxoZh",
+        "https://drive.google.com/uc?export=view&id=1dORPuoyTSEBaoe4h5YrxUg7sC90CobJp",
+        "https://drive.google.com/uc?export=view&id=17998ShQ15LqeGicCeiXwrwfYK5qlyBj8",
+        "https://drive.google.com/uc?export=view&id=1D6dLybA8npmUHUUNaMz8scGz2GjAauNW",
+        "https://drive.google.com/uc?export=view&id=1hiMUvEbw6fcOFl0CajFc09KXhrfjab7U"
       ],
-      description:
-        "lorem ipsum dolor sit amet, consectetur adipis cillum dolore magna aliqu Lorem ipsum dolor sit amet, consectet  durit esse cillum dolore magna aliqu Lorem,lorem ipsum dolor sit amet, consectetur adipislorem ipsum dolor sit amet, consectetur adipis cillum dolore magna aliqu Lorem ipsum dolor sit amet, consectet  durit esse cillum dolore magna aliqu Lorem cillum dolore magna aliqu Lorem ipsum dolor sit amet, consectet  durit esse cillum dolore magna aliqu Lorem",
-    },
-    { id: 2,
-      name: "Quantum Urban Oasis",
-      linkForDrive: "https://www.googledrive.com/lopesarchitecture/quantum",
-      images: [
-        "https://images6.alphacoders.com/617/617254.jpg",
-        "https://images4.alphacoders.com/177/thumb-1920-177016.jpg",
-        "https://avatarfiles.alphacoders.com/116/116794.jpg",
-        "https://images6.alphacoders.com/617/617254.jpg",
-        "https://images4.alphacoders.com/177/thumb-1920-177016.jpg",
-        "https://avatarfiles.alphacoders.com/116/116794.jpg",
-      ],
-      description:
-        "lorem ipsum dolor sit amet, consectetur adipis cillum dolore magna aliqu Lorem ipsum dolor sit amet, consectet  durit esse cillum dolore magna aliqu Lorem",
-    },
-    { id: 3,
-      name: "Serenity Skyscape Tower",
-      linkForDrive: "https://www.googledrive.com/lopesarchitecture/serenity",
-      images: [
-        "https://avatarfiles.alphacoders.com/116/116794.jpg",
-        "https://images6.alphacoders.com/617/617254.jpg",
-        "https://images4.alphacoders.com/177/thumb-1920-177016.jpg",
-        "https://avatarfiles.alphacoders.com/116/116794.jpg",
-        "https://images6.alphacoders.com/617/617254.jpg",
-        "https://images4.alphacoders.com/177/thumb-1920-177016.jpg",
-      ],
-      description:
-        "lorem ipsum dolor sit amet, consectetur adipis cillum dolore magna aliqu Lorem ipsum dolor sit amet, consectet  durit esse cillum dolore magna aliqu Lorem",
-    },
-    { id: 4,
-      name: "Nexus Innovation Hub",
-      linkForDrive: "https://www.googledrive.com/lopesarchitecture/Nexus",
-      images: [
-        "https://images3.alphacoders.com/168/168629.jpg",
-        "https://avatarfiles.alphacoders.com/116/116791.jpg",
-        "https://images5.alphacoders.com/484/thumb-1920-484403.jpg",
-        "https://images3.alphacoders.com/168/168629.jpg",
-        "https://avatarfiles.alphacoders.com/116/116791.jpg",
-        "https://images5.alphacoders.com/484/thumb-1920-484403.jpg",
-      ],
-      description:
-        "lorem ipsum dolor sit amet, consectetur adipis cillum dolore magna aliqu Lorem ipsum dolor sit amet, consectet  durit esse cillum dolore magna aliqu Lorem",
-    },
-    { id: 5,
-      name: "Elisian Heights Residence",
-      linkForDrive: "https://www.googledrive.com/lopesarchitecture/elisian",
-      images: [
-        "https://avatarfiles.alphacoders.com/116/116794.jpg",
-        "https://images6.alphacoders.com/617/617254.jpg",
-        "https://images4.alphacoders.com/177/thumb-1920-177016.jpg",
-        "https://avatarfiles.alphacoders.com/116/116794.jpg",
-        "https://images6.alphacoders.com/617/617254.jpg",
-        "https://images4.alphacoders.com/177/thumb-1920-177016.jpg",
-      ],
-      description:
-        "lorem ipsum dolor sit amet, consectetur adipis cillum dolore magna aliqu Lorem ipsum dolor sit amet, consectet  durit esse cillum dolore magna aliqu Lorem",
-    },
-    { id: 6,
-      name: "Quantum Urban Oasis",
-      linkForDrive: "https://www.googledrive.com/lopesarchitecture/quantum",
-      images: [
-        "https://images5.alphacoders.com/484/thumb-1920-484403.jpg",
-        "https://images3.alphacoders.com/168/168629.jpg",
-        "https://avatarfiles.alphacoders.com/116/116791.jpg",
-      ],
-      description:
-        "lorem ipsum dolor sit amet, consectetur adipis cillum dolore magna aliqu Lorem ipsum dolor sit amet, consectet  durit esse cillum dolore magna aliqu Lorem",
-    },
-    { id: 7,
-      name: "Serenity Skyscape Tower",
-      linkForDrive: "https://www.googledrive.com/lopesarchitecture/serenity",
-      images: [
-        "https://avatarfiles.alphacoders.com/116/116794.jpg",
-        "https://images6.alphacoders.com/617/617254.jpg",
-        "https://images4.alphacoders.com/177/thumb-1920-177016.jpg",
-        "https://avatarfiles.alphacoders.com/116/116794.jpg",
-        "https://images6.alphacoders.com/617/617254.jpg",
-        "https://images4.alphacoders.com/177/thumb-1920-177016.jpg",
-      ],
-      description:
-        "lorem ipsum dolor sit amet, consectetur adipis cillum dolore magna aliqu Lorem ipsum dolor sit amet, consectet  durit esse cillum dolore magna aliqu Lorem",
-    },
-    { id: 8,
-      name: "Nexus Innovation Hub",
-      linkForDrive: "https://www.googledrive.com/lopesarchitecture/Nexus",
-      images: [
-        "https://images5.alphacoders.com/484/thumb-1920-484403.jpg",
-        "https://images3.alphacoders.com/168/168629.jpg",
-        "https://avatarfiles.alphacoders.com/116/116791.jpg",
-      ],
-      description:
-        "lorem ipsum dolor sit amet, consectetur adipis cillum dolore magna aliqu Lorem ipsum dolor sit amet, consectet  durit esse cillum dolore magna aliqu Lorem",
-    },
+      description:"O projeto arquitetônico Casa Boa Vista está localizado em São Francisco de Paula, no bairro Explanada, em Minas Gerais. Este projeto tem como premissa a criação de um ambiente residencial que harmonize a funcionalidade e o conforto, respeitando as características naturais do terreno e promovendo um menor custo de execução.A área total do terreno é de 221,5 m², dos quais 130 m² são destinados à área construída, correspondendo a aproximadamente 58,7% do lote. A distribuição do espaço foi planejada para otimizar a circulação interna, mantendo a privacidade dos quartos, além de garantir a iluminação e ventilação naturais em todos os ambientes.A latitude de -20.7 indica uma localização em uma região de clima predominantemente tropical, com estações definidas, alta incidência solar e ventos predominantes do nordeste. Com base nessas condições climáticas, o projeto prioriza a utilização de materiais que ofereçam conforto térmico, além de soluções arquitetônicas como grandes aberturas e o uso de brises para minimizar a incidência direta do sol nos interiores."
+    }
   ];
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -16,23 +16,21 @@ import styles from "../components/sass_components/Projects.module.css";
 import { IProject, listProjects } from "@/app/projects/projects";
 
 const Projects = () => {
- 
   const refProject = useRef<HTMLDivElement[]>([]);
   const refContainerSlide = useRef<HTMLDivElement>(null);
   const [amountProject, setAmountProject] = useState<number>(4);
   const [showModal, setShowModal] = useState<boolean>(false);
   const [projectClicked, setProjectClicked] = useState<IProject | null>(null);
   let animationBorder: Function;
-  
+
   const showDetailsProject = (e: MouseEvent) => {
-    
     const target = e.target as HTMLElement;
     const projectWraper = target.parentElement as HTMLDivElement;
     const projectItem = projectWraper.parentElement as HTMLDivElement;
     const arrowProject = projectItem.childNodes[0].childNodes[2] as SVGElement;
     const heightDefault: string = "55px";
-    const fullHeight: string = "37vh";
-    
+    const fullHeight: string = "109vh";
+
     if (window.getComputedStyle(projectItem).height === heightDefault) {
       projectItem.style.setProperty("height", `${fullHeight}`);
       arrowProject.style.setProperty("rotate", "180deg");
@@ -41,12 +39,12 @@ const Projects = () => {
       arrowProject.style.setProperty("rotate", "0deg");
     }
   };
-  
-  
+
   animationBorder = (indexElement: number, border: string) => {
-    const borderElementAnimation = refProject.current[indexElement].childNodes[4] as HTMLDivElement;
+    const borderElementAnimation = refProject.current[indexElement]
+      .childNodes[4] as HTMLDivElement;
     borderElementAnimation.style.setProperty("width", "0px");
-    
+
     setTimeout(() => {
       if (border === "increase") {
         borderElementAnimation.style.setProperty("width", "100%");
@@ -54,51 +52,47 @@ const Projects = () => {
         borderElementAnimation.style.setProperty("width", "0px");
       }
     }, 100);
-
-
   };
 
-
- const defineOpacityImage = (action: string,element: Element) => {
-  if(element instanceof HTMLElement){
-    if(action === 'remove'){
-      element.style.setProperty('opacity','0')
-    }else if(action === 'add'){
-      element.style.setProperty('opacity','1')
+  const defineOpacityImage = (action: string, element: Element) => {
+    if (element instanceof HTMLElement) {
+      if (action === "remove") {
+        element.style.setProperty("opacity", "0");
+      } else if (action === "add") {
+        element.style.setProperty("opacity", "1");
+      }
     }
-  }
+  };
 
- }
+  const alternateBetweenImages = () => {
+    const imageList = refContainerSlide.current?.children as HTMLCollection;
+    let index: number = 0;
+    let indexMax: number = imageList!.length - 1;
 
- const alternateBetweenImages = () => {
-  const imageList = refContainerSlide.current?.children as HTMLCollection
-  let index: number = 0;
-  let indexMax: number = imageList!.length - 1;
-
-  imageList![index].classList.add("selected");
-  defineOpacityImage('add', imageList![index])
-
-   setInterval(() => {
-    imageList![index].classList.remove("selected");
-    defineOpacityImage('remove', imageList![index])
-    index++;
-    if(index > indexMax) index = 0
     imageList![index].classList.add("selected");
-    defineOpacityImage('add', imageList![index])
-   },3000)
- }
+    defineOpacityImage("add", imageList![index]);
 
- useEffect(() => {
-  refProject.current = refProject.current.slice(0, listProjects.length);
-  initAnimationsAos();
-  alternateBetweenImages()
- },[])
+    setInterval(() => {
+      imageList![index].classList.remove("selected");
+      defineOpacityImage("remove", imageList![index]);
+      index++;
+      if (index > indexMax) index = 0;
+      imageList![index].classList.add("selected");
+      defineOpacityImage("add", imageList![index]);
+    }, 3000);
+  };
 
+  useEffect(() => {
+    refProject.current = refProject.current.slice(0, listProjects.length);
+    initAnimationsAos();
+    alternateBetweenImages();
+  }, []);
 
   return (
     <div className={styles.projects} id="projects">
       <h4 className={styles.title_session_projects}>
-        <span className={styles.number_session_projects}>1</span>Projetos recentes
+        <span className={styles.number_session_projects}>1</span>Projetos
+        recentes
       </h4>
 
       <div className={styles.session_projects}>
@@ -120,7 +114,12 @@ const Projects = () => {
                 data-aos="fade-right"
               >
                 <div className={styles.wraper_project}>
-                  <h2 onClick={showDetailsProject} className={styles.name_project}>{item.name}</h2>
+                  <h2
+                    onClick={showDetailsProject}
+                    className={styles.name_project}
+                  >
+                    {item.name}
+                  </h2>
 
                   <div
                     onClick={(e) => showDetailsProject(e)}
@@ -129,12 +128,36 @@ const Projects = () => {
                   ></div>
                   <MdOutlineKeyboardArrowDown className={styles.arrow} />
                 </div>
-
-                <p className={styles.description_project}>{item.description}</p>
+                <div className={styles.headline}>
+                  <div className={styles.item_headline}>
+                    <span><b>Localização:</b> {item.location}</span>
+                  </div>
+                  <div className={styles.item_headline}>
+                    <span><b>Latitude:</b> {item.lat}</span>
+                  </div>
+                  <div className={styles.item_headline}>
+                    <span><b>Área do Terreno:</b> {item.terrain_area}</span>
+                  </div>
+                  <div className={styles.item_headline}>
+                    <span><b>Área Construída:</b> {item.build_area}</span>
+                  </div>
+                  <div className={styles.item_headline}>
+                    <span><b>Área permeável:</b> {item.permeable_area}</span>
+                  </div>
+                  <div className={styles.item_headline}>
+                    <span><b>Software:</b> {item.software}</span>
+                  </div>
+                  <div className={styles.item_headline}>
+                    <p className={styles.description_project}>
+                      <b>Descrição: </b>
+                      {item.description}
+                    </p>
+                  </div>
+                </div>
 
                 <span
                   onClick={() => {
-                    setShowModal(true)
+                    setShowModal(true);
                     setProjectClicked(item);
                   }}
                   className={styles.view_image}
@@ -142,7 +165,7 @@ const Projects = () => {
                   Ver imagens
                 </span>
 
-                <Link href={item.linkForDrive}>
+                <Link href={item.linkForDrive} target="_blank">
                   <FaDownload />
                   Baixar no drive
                 </Link>
@@ -152,38 +175,38 @@ const Projects = () => {
             );
           })}
 
-          {showModal && <ModalImages setShowModal={setShowModal} project={projectClicked} />}
+          {showModal && (
+            <ModalImages setShowModal={setShowModal} project={projectClicked} />
+          )}
 
           <div
             onClick={() => setAmountProject(amountProject + 2)}
             className={styles.buttonAllProjects}
           >
-            <ButtonAllProjects valueBtn="Ver Mais" />
+            {/* <ButtonAllProjects valueBtn="Ver Mais" /> */}
           </div>
         </div>
 
-        <div ref={refContainerSlide} data-aos="fade-left" className={styles.slide_projects}>
+        <div
+          ref={refContainerSlide}
+          data-aos="fade-left"
+          className={styles.slide_projects}
+        >
           <Image
             className={styles.selected}
-            src="/images/project_1/img_r4.jpg"
+            src="https://drive.google.com/uc?export=view&id=1abNiPIwTd6IIUilszRTNH8Isrrx7ZY4l"
             width={500}
             height={500}
             alt="Imagem de projeto"
           />
           <Image
-            src="/images/project_1/img_r2.jpg"
+            src="https://drive.google.com/uc?export=view&id=1YOLkJH2d3WGmqNlcXhOSQC_8m_myLuTZ"
             width={500}
             height={500}
             alt="Imagem de projeto"
           />
           <Image
-            src="/images/project_1/img_r3.jpg"
-            width={500}
-            height={500}
-            alt="Imagem de projeto"
-          />
-          <Image
-            src="/images/project_1/img_r1.jpg"
+            src="https://drive.google.com/uc?export=view&id=1I_ozzGbvd_2nVNh-zKsKhZQwEaItFy46"
             width={500}
             height={500}
             alt="Imagem de projeto"

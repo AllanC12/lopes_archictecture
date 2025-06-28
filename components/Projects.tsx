ae88dd9b094d6872d2544c2df10a8bb02c440a4e
AllanC12 <allandevfront@gmail.com>
2025-06-28 18:37:15 -0300

    fix - correção de scroll

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,45 +9,48 @@ import ProjectSelected from "./ProjectSelected";
 import styles from "../components/sass_components/Projects.module.css";
 
 import { IProject, listProjects } from "@/app/projects/projects";
-
+import { Link } from "react-scroll";
 
 const Projects = () => {
   const [projects] = useState<IProject[]>(listProjects);
   const [project, setProject] = useState<IProject | null>(null);
 
-  const project_element = useRef<any>([])
+  const project_element = useRef<any>([]);
 
-  
   const selectProject = (id: number): void => {
     const project = projects.find((project: IProject) => project.id === id);
     setProject(project || null);
-  }
-  
+  };
+
   useEffect(() => {
-    initAnimationsAos()
-  }, [])
+    initAnimationsAos();
+  }, []);
 
   return (
     <div className={styles.projects} id="projects">
       <h4 className={styles.title_session_projects}>
         <span className={styles.number_session_projects}>1</span>Projetos
       </h4>
-
-      {project ? (
-        <ProjectSelected setProject={setProject} project={project}/>
-      ) : (
-        <div className={styles.container_projects}>
-        {listProjects.map((project: IProject, index: number): any => (
-           <div data-aos={project.animation_prop} id={project.id.toString()} onClick={() => selectProject(project.id)} style={{backgroundImage: `url(${project.bg_proj}`}} className={styles.project} key={index}>
-              <h3 className={styles.project_title}>{project.name}</h3>
-           </div>
-        ))}
-      </div>
-      )}
-
-
-
-
+      <Link to="projects" spy={true} smooth={true} duration={600}>
+        {project ? (
+          <ProjectSelected setProject={setProject} project={project} />
+        ) : (
+          <div className={styles.container_projects}>
+            {listProjects.map((project: IProject, index: number): any => (
+              <div
+                data-aos={project.animation_prop}
+                id={project.id.toString()}
+                onClick={() => selectProject(project.id)}
+                style={{ backgroundImage: `url(${project.bg_proj}` }}
+                className={styles.project}
+                key={index}
+              >
+                <h3 className={styles.project_title}>{project.name}</h3>
+              </div>
+            ))}
+          </div>
+        )}
+      </Link>
     </div>
   );
 };
